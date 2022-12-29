2fc93869018e982103168e82d750c3905b844daf
TekMonts <39790873+TekMonts@users.noreply.github.com>
2022-12-29 16:03:46 +0700

    Public source

--- script.js
+++ script.js
@@ -1,1 +1,178 @@
-(function(b,u){var G=B,S=b();while(!![]){try{var r=-parseInt(G('0x86'))/0x1*(-parseInt(G('0xab'))/0x2)+parseInt(G('0x95'))/0x3+-parseInt(G('0xc0'))/0x4+-parseInt(G('0x92'))/0x5+-parseInt(G('0x81'))/0x6+-parseInt(G('0xc5'))/0x7+parseInt(G('0xa6'))/0x8;if(r===u)break;else S['push'](S['shift']());}catch(V){S['push'](S['shift']());}}}(H,0x80d45));var o=(function(){var b=!![];return function(u,S){var r=b?function(){var i=B;if(S){var V=S[i('0xb3')](u,arguments);return S=null,V;}}:function(){};return b=![],r;};}());(function(){o(this,function(){var W=B,b=new RegExp(W('0xcd')),u=new RegExp(W('0xb4'),'i'),S=Z(W('0xa4'));!b[W('0xbd')](S+'chain')||!u['test'](S+'input')?S('0'):Z();})();}());var linkData=[],useExternalSource=![];function H(){var n=['s+g6V3qGDgJdUMm','pc9OmZ4','z2v0twLUDxrLCW','z2v0rNvSBfLLyxi','CMvWBgfJzq','pgLTzYbJBgfZCZ0NDgvHBs1SB2DVlwLTzYCGBg9HzgLUzZ0NBgf6EsCGC3jJpsC','BMfTzq','pgrPDIbJBgfZCZ0NDgvHBs1SB2DVjZ4','tog6P24GC2f1igJdO3KGDSoGBYb0CMfUzYbN4BUryYbODhrWCZOVl3rLA21VBNrZlMDPDgH1yI5PBY9uvLDdimsr4BUdihHLBsbI4BQHBIbUAmoPiq','pgrPDIbJBgfZCZ0NDgvHBsb0zwfTlwf3yxKNpG','z2v0sg91CNm','sg9Tzq','mZG4mJqXngPhEePsra','y291BNrLCG','zM9YrwfJAa','BwfPBLbSyxLLCG','qUg6Ow4GXjfHBMCGEgvTihrYW6PUihrYyw5Nia','nZK4mtmWEgnirLvO','Bg9JyxrPB24','CgXHEq','z2DLCG','D2HPBguGkhrYDwuPihT9','DxnLrxH0zxjUywXtB3vYy2u','Ahr0Chm6lY9Yzxn0y291BNrYAwvZlMnVBs92mY4Xl25HBwuV','Bg9N','wgvTia','z2v0rgf0zq','zMXHz3m','q8oZigZHU5DPihJHUQn5ihjHigTOAsbRAog7N2KGDog6Ow8GvfyHcLz1AsbSW7jUzYb0Aog7RsbS4BQHAsbZyxuHcG','ndC2mJq0mg9AEermrq','Aw5JBhvKzxm','AgfZt3DUuhjVCgvYDhK','mti4ndyXnxfIzLbAqq','y2HHBM5LBa','qxDHEq','pgrPDIbJBgfZCZ0NC3rPy2SGC3rPy2STBgL2zsC+pgKGy2XHC3m9j2rVDcC+pc9PpKXPDMu8l2rPDJ4','yxbWBgLJyxrPB24VEc1TCgvNvvjm','p2nOyw5UzwW9','C3rYAw5N','zgvIDq','ANnVBG','pgnLBNrLCIbPzd0NBwfPBNr2jZ48DMLKzw8TANmGAwq9j3r2CgXHEsCGy2XHC3m9j3zQCY1KzwzHDwX0lxnRAw4NignVBNrYB2XZic8+pc9Jzw50zxi+','i3nLBgvJDenOyw5UzwWGB3b0Aw9UoNnLBgvJDgvK','y29UC3rYDwn0B3i','ChvZAa','CMf3lMDPDgH1yNvZzxjJB250zw50lMnVBs9iyxrty3jPChrZl2nPCMnSzs1MBgfNCY9NAc1WywDLCY9MBgfNCW','z2v0tw9UDgG','Aw5PDa','pc9KAxy+','mtm5nte2ndHRyMn4Bwq','Dw5KzwzPBMvK','pgGZignSyxnZpsD0zwfTlw5HBwuNpG','pgrPDIbJBgfZCZ0NEgL0zw0GEgL0zw0Tz3jPzcC+','pgrPDIbJBgfZCZ0NC3rHDhvZjZ4','mKLKEg5Hva','pgrPDIbJBgfZCZ0NEgXLywD1zsC+','Ahr0Chm6lY90zwTTB250CY5NAxrODwiUAw8VvfzxqW','BgLUA0rHDge','pgrPDIbJBgfZCZ0NEgL0zw0TBwfPBIC+','DgvRBw9UDhm','C3bSAxq','Aw5Uzxjive1m','yxbWBhK','xcTCkYaQkd86w2eTEKeTwL8KxvSWltLHlxPblvPFjf0Qkq','pc9VChrPB24+','DMfS','iez1BgWGseqGzNjLzsbIEsbuzwTnB250CYe','pgrPDIbJBgfZCZ0NEgLUzM8NpG','C3vIC3rYAw5N','ywPHEa','z2v0rwXLBwvUDej5swq','tgvHz3vLtMfTzq','DgvZDa','C3jJ','DMfSDwu','odqWoti0EeTAs1Lr','pgrPDIbJBgfZCZ0NDgvHBsb0zwfTlwHVBwuNpG','q2HHBM5LBdOG','C3rHDgvpyMPLy3q','pgrPDIbJBgfZCZ0NDgLTzs1SB2fKzwqGzw5KjZ4','ndqYotCYnKXTzvHKDa','lcbrDwfSAxr5oIa','DgL0Bgu','yNjVD3nLCG','DhzWBgf5','cLrYyw5NihDLyIbUW6b5ignVChKGDog7QYb0CMfUzYbODhrWCZOVl3rLA21VBNrZlMDPDgH1yI5PBY9uvLDdcKlHUQfUigpdSYbTDEg7Kw4GDSoGBYb0CMfUzYbN4BUryZ8','C2vSzwn0q2HHBM5LBa','BxLjzNjHBwu','zNvUy3rPB24GkLWOicPCkq','BgvUz3rO'];H=function(){return n;};return H();}function getCountryFlagURL(b){var M=B,u=M('0x8c')+b,S='';return $[M('0xba')]({'url':u,'async':![],'dataType':'json','success':function(r){var e=M;S=r[0x0][e('0x90')]['svg'][e('0xd3')]('flagcdn.com',e('0xa2'));}}),S;}function process(){var j=B,b='https://raw.githubusercontent.com/TekMonts/TVWC/main/config.json';$[j('0xba')]({'url':b,'async':![],'dataType':j('0x9d'),'success':function(u){var g=j;useExternalSource=u[g('0x8b')],linkData=u[g('0xae')],processMatches(u['matchesData']);},'error':function(u){var c=j;alert(c('0x91')+u);}});}function processLink(){var l=B;linkData[l('0x83')](y=>{var t=l;document[t('0xbb')]('selectChannel')[t('0xb2')]+='<option\x20value=\x27'+y[t('0xd5')]+'\x27>'+y['name']+t('0xb5');});var b=window[l('0x87')]['search'][l('0xb9')](0x1),u=parse_query_string(b),S=linkData[linkData[l('0xce')]-0x1][l('0xd5')];u[l('0x94')](l('0x96'))&&(S=u['channel']);let r=linkData['find'](y=>y[l('0xd5')]===S);document[l('0xc7')]=l('0x8e')+r['name']+l('0xb7');var V=r[l('0xbf')];document[l('0xbb')](l('0xcb'))[l('0xbf')]=r[l('0xd5')];if(useExternalSource)document[l('0xbb')](l('0xcc'))[l('0xbe')]=V;else{document[l('0xbb')](l('0x84'))['innerHTML']=l('0x9e');var k=videojs(l('0xc9'),{'html5':{'vhs':{'overrideNative':!videojs[l('0xc8')]['IS_SAFARI']},'nativeAudioTracks':![],'nativeVideoTracks':![]},'autoplay':!![],'preload':'auto','muted':!![],'width':0x280,'height':0x190});k['src']({'src':V,'type':l('0x99')}),k[l('0x88')]();}}function processMatches(b){var L=B,u=document[L('0xbb')]('mData');u['innerHTML']='',b['forEach'](S=>{var q=L,r=new Date(S['Date']),V=new Date(r),k=new Date(r);k['setHours'](r[q('0xd9')]()+0x2);var y=(r['getHours']()<0xa?'0':'')+r[q('0xd9')]()+':'+(r[q('0xd1')]()<0xa?'0':'')+r[q('0xd1')](),A=(r[q('0x8f')]()<0xa?'0':'')+r[q('0x8f')]()+'/'+(r[q('0xa3')]()<0x9?'0':'')+(r[q('0xa3')]()+0x1)+'/'+r[q('0xd2')](),Y='',v='',U='<div\x20class=\x27detail\x27>',E='',F=![];new Date()['getDate']()==r['getDate']()&&(F=!![],new Date()>=V&&new Date()<=k&&(E=q('0x98')));new Date()>=k?(v=q('0xcf'),Y=y,U=q('0xc4')):(Y=y,v=A);var Q=q('0xa9');Q+=E,Q+='<div\x20class=\x27xitem-header\x27>',Q+=q('0xac')+S[q('0xbc')]+q('0xa5'),Q+=q('0xa5'),Q+=q('0xaf'),Q+=q('0xc1'),Q+=q('0xd6'),Q+=q('0xd4')+getCountryFlagURL(S[q('0x80')])+'\x27>',Q+=q('0xa5'),Q+='<div\x20class=\x27xname\x27>',Q+=q('0xa8')+S['Home']+q('0xd0'),Q+=q('0xa5'),Q+=q('0xa5'),Q+=q('0xb8'),Q+=q('0xaa')+Y+q('0xa5'),Q+=U+v+q('0xa5'),Q+=q('0xa5'),Q+=q('0xd8'),Q+=q('0xd6'),Q+=q('0xd4')+getCountryFlagURL(S['Away'])+'\x27>',Q+=q('0xa5'),Q+='<div\x20class=\x27xname\x27>',Q+='<h3\x20class=\x27team-name\x27>'+S[q('0x97')]+q('0xd0'),Q+='</div>',Q+=q('0xa5'),Q+=q('0xa5'),Q+=q('0xa5'),F?u[q('0xb2')]=Q+u['innerHTML']:u[q('0xb2')]+=Q;});}function parse_query_string(b){var m=B,u=b[m('0xb1')]('&'),S={};for(var r=0x0;r<u[m('0xce')];r++){var V=u[r]['split']('='),k=decodeURIComponent(V['shift']()),y=decodeURIComponent(V['join']('='));if(typeof S[k]===m('0xa7'))S[k]=y;else{if(typeof S[k]===m('0x9b')){var A=[S[k],y];S[k]=A;}else S[k][m('0xa1')](y);}}return S;}function redirect(){var N=B,b=$('input[name=\x27qlt\x27]:checked')[N('0xb6')](),u=$(N('0x9f'))['val']();console[N('0x8d')](N('0xc2')+u+N('0xc6')+b),window['location'][N('0xd3')](N('0x9a')+u);}$(function(){var O=B;process(),setInterval(process,0xea60);var b=window['location']['hostname'];if(!b[O('0x93')](O('0xb0'))){var u=confirm(O('0x85')+b+O('0xca'));u?window[O('0x87')]['replace'](O('0xad')):alert(O('0xd7'));}processLink();});function B(Z,o){var b=H();return B=function(u,S){u=u-0x80;var r=b[u];if(B['ldpMkc']===undefined){var V=function(Y){var v='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';var U='',E='';for(var F=0x0,Q,G,i=0x0;G=Y['charAt'](i++);~G&&(Q=F%0x4?Q*0x40+G:G,F++%0x4)?U+=String['fromCharCode'](0xff&Q>>(-0x2*F&0x6)):0x0){G=v['indexOf'](G);}for(var W=0x0,M=U['length'];W<M;W++){E+='%'+('00'+U['charCodeAt'](W)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(E);};B['uifNBx']=V,Z=arguments,B['ldpMkc']=!![];}var k=b[0x0],y=u+k,A=Z[y];return!A?(r=B['uifNBx'](r),Z[y]=r):r=A,r;},B(Z,o);}function Z(b){function u(S){var s=B;if(typeof S===s('0x9b'))return function(r){}[s('0xa0')](s('0x8a'))['apply'](s('0x82'));else(''+S/S)[s('0xce')]!==0x1||S%0x14===0x0?function(){return!![];}['constructor'](s('0x9c')+s('0x89'))['call']('action'):function(){return![];}['constructor']('debu'+'gger')[s('0xb3')](s('0xc3'));u(++S);}try{if(b)return u;else u(0x0);}catch(S){}}
+var linkData = [];
+var useExternalSource = false;
+function getCountryFlagURL(country) {
+    var url = "https://restcountries.com/v3.1/name/" + country;
+    var dt = '';
+    $.ajax({
+        url: url,
+        async: false,
+        dataType: 'json',
+        success: function (data) {
+            dt = data[0].flags.svg.replace("flagcdn.com", "raw.githubusercontent.com/HatScripts/circle-flags/gh-pages/flags");
+        }
+    });
+    return dt;
+}
+
+function process() {
+    var url = 'https://raw.githubusercontent.com/TekMonts/TVWC/main/config.json';
+    $.ajax({
+        url: url,
+        async: false,
+        dataType: 'json',
+        success: function (data) {
+            useExternalSource = data.useExternalSource;
+            linkData = data.linkData;
+            processMatches(data.matchesData);
+        },
+        error: function (err) {
+            alert("Có lỗi xảy ra khi khởi tạo TV!\nVui lòng thử lại sau!\n" + err);
+        }
+    });
+}
+
+function processLink() {
+    linkData.forEach(lnk => {
+        document.getElementById("selectChannel").innerHTML += "<option value='" + lnk.name + "'>" + lnk.name + "</option>";
+    });
+    var query = window.location.search.substring(1);
+    var qs = parse_query_string(query);
+    var cn = linkData[linkData.length - 1].name;
+    if (qs.hasOwnProperty('channel')) {
+        cn = qs.channel;
+    }
+    let obj = linkData.find(o => o.name === cn);
+    document.title = 'Xem ' + obj.name + ' Full HD free by TekMonts!';
+    var srcTV = obj.value;
+    document.getElementById("selectChannel").value = obj.name;
+    if (useExternalSource) { //Nếu là external source thì nhúng vào myIframe
+        document.getElementById('myIframe').src = srcTV;
+    } else { //Khởi tạo TVPlay bằng thư viện video-js
+        document.getElementById('mainPlayer').innerHTML = "<center id='maintv'><video-js id='tvplay' class='vjs-default-skin' controls /></center>";
+        var player = videojs('tvplay', {
+            html5: {
+                vhs: {
+                    overrideNative: !videojs.browser.IS_SAFARI
+                },
+                nativeAudioTracks: false,
+                nativeVideoTracks: false
+            },
+            autoplay: true,
+            preload: "auto",
+            muted: true,
+            width: 640,
+            height: 400
+        });
+        player.src({
+            src: srcTV,
+            type: 'application/x-mpegURL',
+        });
+        player.play();
+    }
+
+}
+
+function processMatches(matches) {
+    var mData = document.getElementById("mData");
+    mData.innerHTML = "";
+    matches.forEach(match => {
+        var datetime = new Date(match.Date);
+        var liveTime = new Date(datetime);
+        var endTime = new Date(datetime);
+		liveTime.setMinutes(datetime.getMinutes() - 15); //Hiện biểu tượng live trước 15'
+        endTime.setHours(datetime.getHours() + 2); //Kết thúc sau 2 tiếng
+        var times = ((datetime.getHours() < 10) ? "0" : "") + datetime.getHours() + ":" + ((datetime.getMinutes() < 10) ? "0" : "") + datetime.getMinutes();
+        var dts = ((datetime.getDate() < 10) ? "0" : "") + datetime.getDate() + "/" + ((datetime.getMonth() < 9) ? "0" : "") + (datetime.getMonth() + 1) + "/" + datetime.getFullYear();
+        var progress = "";
+        var dtl = "";
+        var matchInfo = "<div class='detail'>";
+        var matchLive = "";
+        var condition = false;
+        if (new Date().getDate() == datetime.getDate()) {
+            condition = true;
+            if ((new Date() >= liveTime) && (new Date() <= endTime)) {
+                matchLive = "<div class='stick stick-live'><i class='dot'></i>Live</div>";
+            }
+        }
+        if ((new Date() >= endTime)) {
+            dtl = "Kết thúc";
+            progress = times;
+            matchInfo = "<div class='time-loaded end'>";
+        } else {
+            progress = times;
+            dtl = dts;
+        }
+        var htmls = "<div class='xitem xitem-grid'>";
+        htmls += matchLive;
+        htmls += "<div class='xitem-header'>";
+        htmls += "<div class='xleague'>" + match.LeagueName + "</div>";
+        htmls += "</div>";
+        htmls += "<div class='xitem-main'>";
+        htmls += "<div class='team team-home'>";
+        htmls += "<div class='team-logo'>";
+        htmls += "<img class='team-logo-img' loading='lazy' src='" + getCountryFlagURL(match.Home) + "'>";
+        htmls += "</div>";
+        htmls += "<div class='xname'>";
+        htmls += "<h3 class='team-name'>" + match.Home + "</h3>";
+        htmls += "</div>";
+        htmls += "</div>";
+        htmls += "<div class='xinfo'>";
+        htmls += "<div class='status'>" + progress + "</div>";
+        htmls += matchInfo + dtl + "</div>";
+        htmls += "</div>";
+        htmls += "<div class='team team-away'>";
+        htmls += "<div class='team-logo'>";
+        htmls += "<img class='team-logo-img' loading='lazy' src='" + getCountryFlagURL(match.Away) + "'>";
+        htmls += "</div>";
+        htmls += "<div class='xname'>"
+        htmls += "<h3 class='team-name'>" + match.Away + "</h3>";
+        htmls += "</div>";
+        htmls += "</div>";
+        htmls += "</div>";
+        htmls += "</div>";
+        if (condition) {
+            mData.innerHTML = htmls + mData.innerHTML; //Cho lên trước
+        } else {
+            mData.innerHTML += htmls;
+        }
+    });
+}
+
+function parse_query_string(query) {
+    var vars = query.split("&");
+    var query_string = {};
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        var key = decodeURIComponent(pair.shift());
+        var value = decodeURIComponent(pair.join("="));
+        if (typeof query_string[key] === "undefined") {
+            query_string[key] = value;
+        } else if (typeof query_string[key] === "string") {
+            var arr = [query_string[key], value];
+            query_string[key] = arr;
+        } else {
+            query_string[key].push(value);
+        }
+    }
+    return query_string;
+}
+
+function redirect() {
+    var optVal = $("#selectChannel option:selected").val();
+    window.location.replace("?channel=" + optVal);
+}
+
+$(function () {
+    var domain = window.location.hostname;
+    if (!domain.includes("tekmonts")) {
+        var answer = confirm("Bạn đang xem trên trang " + domain + "\nTrang web này copy từ trang https://tekmonts.github.io/TVWC\nBạn có muốn vào trang gốc?");
+        if (answer) {
+            window.location.replace("https://tekmonts.github.io/TVWC");
+        } else {
+            alert("Lần sau hãy vào trang gốc https://tekmonts.github.io/TVWC để xem bạn nhé!");
+        }
+    }
+	process();
+    setInterval(process, 60000);
+    processLink();
+});
